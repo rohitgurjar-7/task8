2c73cafd38219ea045e96a4b883069b98cd3efa6
rohitgurjar0704 <75878942+rohitgurjar0704@users.noreply.github.com>
2021-07-08 00:13:55 +0530

    Add files via upload

--- car.py
+++ car.py
@@ -7,27 +7,14 @@ print()
 f=cgi.FieldStorage()
 cmd = f.getvalue("x")
 
-if cmd == "256 D 259":
-	print('''<pre>
-	Car Number: 256 D 259
-	Car Model: BMW	
-	Registration Name: Ajith
-	Registration Date: 17/1/2013
-	Fuel Type: CNG
-	Location: Delhi, India
-	Vehicle Class: SUV
-	Insurance Upto: 19/12/2022
-	</pre>''')
-elif cmd == "MH02EC 4747":
+if cmd == "MH02EC 4747":
 	print('''<pre>
 	Car Number: MH02EC 4747
 	Car Model: Maserati Quattroporto
 	Registration Name: Sushant Singh Rajput
-	Registration Date: 17/1/2016
 	Fuel Type: Petrol 
 	Location: Haryana , India
-	Vehicle Class: SUV
-	Insurance Upto: 19/12/2026
+	Vehicle Class: Super Sedan
 	</pre>''')
 
 elif cmd == "TTL D4FTY":
